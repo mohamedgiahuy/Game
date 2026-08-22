bbe7cf2841bb8998d6e0828b59fdc2fd8a1d53ee
mohamedgiahuy <giahuy06122k11@gmail.com>
2026-08-22 15:32:50 +0700

    day1

---
+++ Assets/Script/PlrMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float walkSpeed = 3.5f;
+    [SerializeField] private float runSpeed = 6f;
+    [SerializeField] private float gravity = -20f;
+
+    [Header("Mouse Look")]
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private float mouseSensitivity = 2f;
+    [SerializeField] private float maxLookAngle = 85f;
+
+    private CharacterController controller;
+    private float verticalVelocity;
+    private float cameraPitch;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Move();
+        Look();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction =
+            transform.right * x +
+            transform.forward * z;
+
+        direction.Normalize();
+
+        float speed = Input.GetKey(KeyCode.LeftShift)
+            ? runSpeed
+            : walkSpeed;
+
+        if (controller.isGrounded && verticalVelocity < 0)
+            verticalVelocity = -2f;
+
+        verticalVelocity += gravity * Time.deltaTime;
+
+        Vector3 velocity = direction * speed;
+        velocity.y = verticalVelocity;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        // Xoay người sang trái/phải
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Xoay camera lên/xuống
+        cameraPitch -= mouseY;
+        cameraPitch = Mathf.Clamp(
+            cameraPitch,
+            -maxLookAngle,
+            maxLookAngle
+        );
+
+        playerCamera.localRotation =
+            Quaternion.Euler(cameraPitch, 0f, 0f);
+    }
+}
