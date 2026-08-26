fd00eefd6a1b1c0a6b26e091bdd1e4c8e4f28655
mohamedgiahuy <giahuy06122k11@gmail.com>
2026-08-26 16:11:11 +0700

    fix jumpscare + model   kiem lam

--- Assets/Script/Jumpscare.cs
+++ Assets/Script/Jumpscare.cs
@@ -4,9 +4,30 @@ using UnityEngine.UI;
 
 public class JumpScare : MonoBehaviour
 {
-    [SerializeField] private RawImage scareImage;
-    [SerializeField] private AudioSource scareSound;
+    public enum ScareType
+    {
+        AudioOnly,
+        UIImage,
+        Object3D,
+        EmptyGroup
+    }
+
+    [Header("Scare Mode Setup")]
+    [SerializeField] private ScareType scareType = ScareType.AudioOnly;
     [SerializeField] private float displayDuration = 0.5f;
+
+    [Header("Audio Settings")]
+    [SerializeField] private AudioSource scareSound;
+
+    [Header("UI Image Mode")]
+    [SerializeField] private RawImage scareImage;
+
+    [Header("3D Object Mode")]
+    [SerializeField] private GameObject scareObject;
+
+    [Header("Empty Group Mode")]
+    [Tooltip("Kéo Empty GameObject (đã tắt sẵn) chứa toàn bộ quái/hiệu ứng vào đây.")]
+    [SerializeField] private GameObject emptyGroup;
 
     private bool hasTriggered = false;
 
@@ -18,16 +39,6 @@ public class JumpScare : MonoBehaviour
             TriggerJumpScare();
         }
     }
-
-    // Test bằng phím Space
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TriggerJumpScare();
-        }
-    }
-
     public void TriggerJumpScare()
     {
         StartCoroutine(JumpScareSequence());
@@ -35,19 +46,49 @@ public class JumpScare : MonoBehaviour
 
     private IEnumerator JumpScareSequence()
     {
-        // Bật image
-        scareImage.gameObject.SetActive(true);
+        // 1. Bật đối tượng tương ứng theo tùy chọn
+        switch (scareType)
+        {
+            case ScareType.UIImage:
+                if (scareImage != null) scareImage.gameObject.SetActive(true);
+                break;
 
-        // Phát âm thanh
+            case ScareType.Object3D:
+                if (scareObject != null) scareObject.SetActive(true);
+                break;
+
+            case ScareType.EmptyGroup:
+                if (emptyGroup != null) emptyGroup.SetActive(true);
+                break;
+
+            case ScareType.AudioOnly:
+            default:
+                break;
+        }
+
+        // 2. Phát âm thanh
         if (scareSound != null)
         {
             scareSound.Play();
         }
 
-        // Giữ trong khoảng thời gian
+        // 3. Giữ trong khoảng thời gian
         yield return new WaitForSeconds(displayDuration);
 
-        // Tắt image
-        scareImage.gameObject.SetActive(false);
+        // 4. Tắt đối tượng sau khi kết thúc hù dọa
+        switch (scareType)
+        {
+            case ScareType.UIImage:
+                if (scareImage != null) scareImage.gameObject.SetActive(false);
+                break;
+
+            case ScareType.Object3D:
+                if (scareObject != null) scareObject.SetActive(false);
+                break;
+
+            case ScareType.EmptyGroup:
+                if (emptyGroup != null) emptyGroup.SetActive(false);
+                break;
+        }
     }
 }
